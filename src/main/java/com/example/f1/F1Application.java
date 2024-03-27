eff9ec0c8365c5ede52f2a21ee77010a8d145092
Wiktor <wiktorszulc4@gmail.com>
2024-03-27 22:14:28 +0100

    start

---
+++ src/main/java/com/example/f1/F1Application.java
@@ -0,0 +1,13 @@
+package com.example.f1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/f1/F1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.f1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
