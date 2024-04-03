7713aeaada76a88cf3f73162760660e2b5fe2cc4
Wiktor <wiktorszulc4@gmail.com>
2024-04-03 21:43:53 +0200

    test: add test team

--- src/main/java/com/example/f1/entity/Team.java
+++ src/main/java/com/example/f1/entity/Team.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/example/f1/team/TeamServiceImplTest.java
+++ src/test/java/com/example/f1/team/TeamServiceImplTest.java
@@ -34,6 +34,10 @@ class TeamServiceImplTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private MockMvc mockMVC;
+    private List<Team> teams = new ArrayList<>(List.of(
+            new Team("Red Bull Racing", "Adrian Newey", List.of(new Driver(), new Driver())),
+            new Team("Scuderia Ferrari", "Mattia Binotto", List.of(new Driver(), new Driver())),
+            new Team("McLaren", "James Key", List.of(new Driver(), new Driver()))));
     @Container
     private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql")
             .withDatabaseName("tests").withUsername("spring").withPassword("spring")
@@ -48,23 +52,46 @@ class TeamServiceImplTest {
         mySQLContainer.start();
     }
     @Test
-    @Order(value = 1)
     void testConnectionToDatabase() {
         Assertions.assertNotNull(teamRepository);
     }
     @Test
-    @Order(value = 2)
     void testAddTeams() throws Exception{
-        List<Team> teams = new ArrayList<>(List.of(
-                new Team("Red Bull Racing", "Adrian Newey", List.of(new Driver(), new Driver())),
-                new Team("Scuderia Ferrari", "Mattia Binotto", List.of(new Driver(), new Driver())),
-                new Team("McLaren", "James Key", List.of(new Driver(), new Driver()))));
         for (Team team : teams){
             String teamJSON = objectMapper.writeValueAsString(team);
             mockMVC.perform(
                     MockMvcRequestBuilders.post("/f1/team").contentType(MediaType.APPLICATION_JSON)
                     .content(teamJSON)).andExpect(status().isOk());
-            Assertions.assertEquals(3, teamRepository.findAll().size());
         }
+        Assertions.assertEquals(3, teamRepository.findAll().size());
+    }
+    @Test
+    void testGetAllTeams() throws Exception{
+        mockMVC.perform(
+                MockMvcRequestBuilders.get("/f1/team")
+        ).andExpect(status().isOk());
+        Assertions.assertEquals(teams.get(1).getName(), teamRepository.findAll().get(1).getName());
+    }
+    @Test
+    void testGetTeamById() throws Exception{
+        mockMVC.perform(
+                MockMvcRequestBuilders.get("/f1/team/1")
+        ).andExpect(status().isOk());
+        Assertions.assertEquals(teams.get(0).getName(), teamRepository.findById(1).get().getName());
+    }
+    @Test
+    void testUpdateTeam() throws Exception{
+        Team team = new Team(1, "Mercedes", "James Allison", List.of(new Driver(), new Driver()));
+        String teamJSON = objectMapper.writeValueAsString(team);
+        mockMVC.perform(
+                MockMvcRequestBuilders.put("/f1/team").contentType(MediaType.APPLICATION_JSON)
+                        .content(teamJSON)).andExpect(status().isOk());
+        Assertions.assertEquals(team.getName(), teamRepository.findById(1).get().getName());
+    }
+    @Test
+    void testDeleteById() throws Exception{
+        mockMVC.perform(
+                MockMvcRequestBuilders.delete("/f1/team/1")
+        ).andExpect(status().isOk());
     }
 }
